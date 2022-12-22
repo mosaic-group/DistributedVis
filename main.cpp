9ef2f0cc270d0c480dedd3208517d9d1c4449c48
Brockaaa <brocka.dev@gmail.com>
2022-12-22 22:21:13 +0100

    22.12.22 22:21:13

--- main.cpp
+++ main.cpp
@@ -165,7 +165,7 @@ int main() {
 
         std::cout<<"Back after calling do Render" <<std::endl;
 
-        sleep(60);
+        sleep(300);
         std::cout<<"Calling stopRendering!" <<std::endl;
         stopRendering(jvmData);
 
